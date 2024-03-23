21d191f0d886e1105c3a8320fbb56c40c04319bb
dongmylinh <117095462+dongmylinh@users.noreply.github.com>
2024-03-23 21:56:47 +0700

    #6 - Create slider banner component

--- Books-Ecommerce-Frontend/src/App.jsx
+++ Books-Ecommerce-Frontend/src/App.jsx
@@ -4,20 +4,22 @@ import { Header } from "./components/Header"
 
 
 import { DetailCart } from "./components/DetailCart";
-import { DescriptionFeedback } from "./components/DescriptionFeedback";
-import { ShoppingCarts } from "./components/ShoppingCarts";
-import { ShoppingCartsPopup } from "./components/ShoppingCartsPopup";
+// import { DescriptionFeedback } from "./components/DescriptionFeedback";
+// import { ShoppingCarts } from "./components/ShoppingCarts";
+// import { ShoppingCartsPopup } from "./components/ShoppingCartsPopup";
 
 
 
 import { Footer } from "./components/Footer";
 import {Navbar} from "./components/Navbar"
+// import Slider from "./components/Slider";
 function App() {
   return (
     <>
 
       <Header/>
       <Navbar/>
+      {/* <Slider/> */}
       <DetailCart />
       <DescriptionFeedback />
       <Footer/>

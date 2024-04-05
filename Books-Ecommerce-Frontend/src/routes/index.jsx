5f22baed456546972177ee71d18563a1a2ab485a
phamvuong2002 <117916286+phamvuong2002@users.noreply.github.com>
2024-04-05 22:06:01 +0700

    #54 delete general page

--- Books-Ecommerce-Frontend/src/routes/index.jsx
+++ Books-Ecommerce-Frontend/src/routes/index.jsx
@@ -3,7 +3,6 @@ import { PaymentPage } from "../pages/PaymentPage";
 import { ShoppingCartsPage } from "../pages/ShoppingCartsPage";
 import { AccountPage } from "../pages/AccountPage";
 import { Home } from "../pages/Home";
-import { GeneralInfomation } from "../pages/GeneralInfomation";
 
 // Publishes Route user foll all users
 const publicRoutes = [
@@ -11,8 +10,7 @@ const publicRoutes = [
     { path: '/books/:bookid', component: ProductDetailPage },
     { path: '/payment', component: PaymentPage },
     { path: '/shoppingcarts', component: ShoppingCartsPage },
-    { path: '/account', component: AccountPage },
-    { path: '/genaral-infomation', component: GeneralInfomation },
+    { path: '/account', component: AccountPage }
 
 ];
 

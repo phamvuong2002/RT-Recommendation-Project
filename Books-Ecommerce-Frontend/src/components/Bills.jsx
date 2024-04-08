6d80fd5925e23132c6954cd050a9118e98327af8
phamvuong2002 <117916286+phamvuong2002@users.noreply.github.com>
2024-04-08 14:05:56 +0700

    #17 add status for total bill

--- Books-Ecommerce-Frontend/src/components/Bills.jsx
+++ Books-Ecommerce-Frontend/src/components/Bills.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { ShoppingCartLoader } from './loaders/ShoppingCartLoader';
 import { SingleBill } from './SingleBill';
+import { determineBillStatus } from '../helpers/detemindBillsStatus';
 
 
 
 const Bills = ({ bills, className = "", setReload }) => {
     const NUMLOADER = 5
+
+    const TOTALBILLSTATUS = {
+        "Completed": { name: "Hoàn thành", bgColor: "bg-green-500", textColor: "bg-green-500" },
+        "Shipping": { name: "Đang vận chuyển", bgColor: "bg-blue-500", textColor: "bg-blue-500" },
+        "Refunded": { name: "Hoàn Trả", bgColor: "bg-red-200", textColor: "bg-red-200" },
+        "Processing": { name: "Đang xử lý", bgColor: "bg-gray-500", textColor: "bg-gray-500" }
+    }
 
     const groupBooksByBillId = (bills) => {
         const groupedBills = {};
@@ -23,6 +31,7 @@ const Bills = ({ bills, className = "", setReload }) => {
     // Group bills by billId
     const groupedBills = groupBooksByBillId(bills);
 
+
     return (
         <div className={className}>
             {
@@ -31,25 +40,34 @@ const Bills = ({ bills, className = "", setReload }) => {
                     :
                     <div className="">
                         {
-                            Object.keys(groupedBills).map(billId => (
-                                <div key={billId} className="">
-                                    <div className="flex items-center gap-3 xl:py-2 xl:h-8 xl:rounded-full h-7 w-full text-white px-1 font-semibold bg-red-500">
-                                        <div className="xl:pl-1"> Số hoá đơn #{billId}</div>
-                                        <div className="xl:pr-1">({groupedBills[billId].length} đơn hàng)</div>
+                            Object.keys(groupedBills).map(billId => {
+                                const status = determineBillStatus(groupedBills[billId]); // Đặt biến status tại vị trí này
+                                return (
+                                    <div key={billId} className="">
+                                        <div className="flex items-center text-xs justify-between xl:text-base gap-3 xl:py-2 xl:h-8 xl:rounded-full h-7 w-full text-white px-1 font-semibold bg-red-500">
+                                            <div className="flex gap-1">
+                                                <div className="xl:pl-1"> Số hoá đơn #{billId}</div>
+                                                <div className="xl:pr-1">({groupedBills[billId].length} đơn hàng)</div>
+                                            </div>
+                                            <div className={`flex items-center bg-gray-400 rounded-full xl:text-sm xl:px-2 xl:items-center`}>  {/*${TOTALBILLSTATUS[status].bgColor} */}
+                                                <div className="flex items-center px-1 h-5 xl:h-5 xl:mb-[0.1rem]">
+                                                    {TOTALBILLSTATUS[status].name}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* Render Swiper here for each group of bills */}
+                                        {groupedBills[billId].map(bill => (
+                                            <SingleBill key={bill.detailBillId} bill={bill} setReload={setReload} />
+                                        ))}
                                     </div>
-                                    {/* Render Swiper here for each group of bills */}
-                                    {groupedBills[billId].map(bill => (
-                                        <SingleBill key={bill.detailBillId} bill={bill} setReload={setReload} />
-                                    ))}
-                                </div>
-                            ))
+                                );
+                            })
                         }
                     </div>
-
-
             }
         </div>
-    )
+    );
+
 }
 
 export default Bills

feafc2dd8186f0ca6032658a9ec44b0318fdaae7
phamvuong2002 <117916286+phamvuong2002@users.noreply.github.com>
2024-04-11 19:25:58 +0700

    #93 setup node js server

---
+++ Books-Ecommerce-Backend/src/app.js
@@ -0,0 +1,94 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const session = require("express-session");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const {
+  app: { session_key, session_name, session_db },
+} = require("./v1/configs/config");
+
+//init middlewares
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// setting up connect-mongodb-session store
+const mongoDBstore = new MongoDBStore({
+  uri: process.env.DATABASE_CONNECTION_STRING,
+  collection: session_db,
+});
+
+//set headers for sending payload
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
+//init session
+app.use(
+  session({
+    secret: session_key,
+    name: session_name, // cookies name to be put in "key" field in postman
+    store: mongoDBstore,
+    cookie: {
+      maxAge: 5 * 60 * 1000, // this is when our cookies will expired and the session will not be valid anymore (user will be log out)
+      sameSite: false,
+      secure: false, // to turn on just in production
+    },
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//init db
+require("./v1/dbs/init.mongo");
+
+//init routes
+app.use("/", require("./v1/routes"));
+
+//handling errors
+app.use((req, res, next) => {
+  const error = new Error("Service Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+module.exports = app;
---
+++ Books-Ecommerce-Backend/src/v1/const/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const ROLES = {
+    CLIENT: '10001', // USER
+    WRITER: '10010', // ALLOW TO WRITE
+    EDITOR: '10110', // ALLOW TO EDIT
+    ADMIN: '10111', // ADMIN
+}
+
+
+
+module.exports = {
+    ROLES,
+}

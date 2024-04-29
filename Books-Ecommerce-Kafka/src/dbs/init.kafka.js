71b1efe4c88bcf72d046eb8308be9d07d2212e2a
phamvuong2002 <117916286+phamvuong2002@users.noreply.github.com>
2024-04-29 17:14:06 +0700

    #153 setup kafka

---
+++ Books-Ecommerce-Kafka/src/dbs/init.kafka.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { Kafka, logLevel } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "bookada-client-collection",
+  brokers: ["localhost:9092"],
+  logLevel: logLevel.NOTHING,
+});
+
+const connectToKafka = async (topics, handleMessage) => {
+  const consumer = kafka.consumer({ groupId: "collect" });
+  try {
+    await consumer.connect();
+
+    // Subscribe to each topic in the list
+    for (const topicKey of Object.keys(topics)) {
+      const topic = topics[topicKey];
+      await consumer.subscribe({ topic, fromBeginning: true });
+    }
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        await handleMessage({ topic, partition, message });
+      },
+    });
+  } catch (error) {
+    console.error("Error in Kafka consumer:", error);
+    throw error;
+  }
+};
+
+module.exports = {
+  connectToKafka,
+};

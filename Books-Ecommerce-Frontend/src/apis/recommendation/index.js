18c9e9aef66a9abac565a23d46f3c929655550e9
dongmylinh <117095462+dongmylinh@users.noreply.github.com>
2024-06-01 11:19:47 +0700

    #190 add api for collaborative filtering

--- Books-Ecommerce-Frontend/src/apis/recommendation/index.js
+++ Books-Ecommerce-Frontend/src/apis/recommendation/index.js
@@ -3,3 +3,11 @@ export const searchbestselling =
   'api/v1/api/recommendation/search-best-selling';
 export const categorybestselling =
   'api/v1/api/recommendation/best-selling/categories';
+
+export const recLatestBook =
+  'api/v1/api/recommendation/recLatestBook';
+export const recRandomBook =
+  'api/v1/api/recommendation/recRandomBook';
+export const behaviour_retrain =
+  'api/v1/api/recommendation/behaviour/retrain_svdpp';
+
